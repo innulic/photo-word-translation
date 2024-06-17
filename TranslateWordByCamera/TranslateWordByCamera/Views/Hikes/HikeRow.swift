3628455b0a9608e9472827cb3d40355232c46f6f
Inna Ankudinova <innulic@Innas-MacBook-Pro.local>
2024-06-17 20:39:25 +0100

    finish swift basics

--- TranslateWordByCamera/TranslateWordByCamera/Views/Hikes/HikeRow.swift
+++ TranslateWordByCamera/TranslateWordByCamera/Views/Hikes/HikeRow.swift
@@ -7,35 +7,49 @@
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
 struct HikeRow: View {
     var hike: Hike
-    @State var showDetails = false
+    @State var showDetails = true
     var body: some View {
-            HStack{
-                HikeGraph(hike: hike, path: \.elevation)
-                    .frame(width: 50, height: 30)
-                
-                VStack(alignment: .leading){
-                    Text(hike.name)
-                        .font(.headline)
-                    Text(hike.distanceText)
-                }
-                
-                Spacer()
-                
-                Button{
+        HStack{
+            HikeGraph(hike: hike, path: \.elevation)
+                .frame(width: 50, height: 30)
+            
+            VStack(alignment: .leading){
+                Text(hike.name)
+                    .font(.headline)
+                Text(hike.distanceText)
+            }
+            
+            Spacer()
+            
+            Button{
+                withAnimation{
                     showDetails.toggle()
-                }label: {
-                    Label("Graph", systemImage: "chevron.right.circle")
-                        .labelStyle(.iconOnly)
-                        .imageScale(.large)
-                        .rotationEffect(.degrees(showDetails ? 90 : 0))
-                        .padding()
                 }
-                
-                if(showDetails){
-                    HikeDetail(hike: hike)
-                }
+            }label: {
+                Label("Graph", systemImage: "chevron.right.circle")
+                    .labelStyle(.iconOnly)
+                    .imageScale(.large)
+                    .rotationEffect(.degrees(showDetails ? 90 : 0))
+                    //.scaleEffect(showDetails ? 1.5 : 1)
+                    .padding()
+                    //.animation(.spring, value: showDetails)
+            }
+        }
+        
+        if(showDetails){
+            HikeDetail(hike: hike)
+                .transition(.slide)
         }
     }
 }
